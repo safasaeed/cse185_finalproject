c57f070e2912574699ea83e8b3441bee0e0b8e9a
safasaeed <126025187+safasaeed@users.noreply.github.com>
2023-05-17 13:37:42 -0700

    Added code to access genotype data in VCF

--- source.py
+++ source.py
@@ -5,24 +5,33 @@ import vcf
 import argparse
 
 def main():
-  parser = argparse.ArgumentParser(
-    prog = "mygwas",
-    description = "Command-line script to perform GWAS linear regression"
-  )
-  
-  # Input
-  # Genotype input file
-  parser.add_argument("vcf", help = "Input vcf file with genotype data", type = str)
-  # Phenotype input file
-  parser.add_argument("phen", help = "Input phen file with phenotype data", type = str)
-  
-  # Output
-  parser.add_argument("-o", "--out", help = "Specify output file name", metavar = "FILE" type = str, required = False)
-  
-  # Parse args
-  args = parser.parse_args()
-  
+    parser = argparse.ArgumentParser(
+      prog = "mygwas",
+      description = "Command-line script to perform GWAS linear regression"
+    )
 
-genotypes = '
+    # Input
+    # Genotype input file
+    parser.add_argument("vcf", help = "Input vcf file with genotype data", type = str)
+    # Phenotype input file
+    parser.add_argument("phen", help = "Input phen file with phenotype data", type = str)
 
+    # Output
+    parser.add_argument("-o", "--out", help = "Specify output file name", metavar = "FILE" type = str, required = False)
 
+    # Parse args
+    args = parser.parse_args()
+
+    # Open VCF file
+    genotypes = args.vcf
+    vcf_reader = vcf.Reader(open(genotypes, 'r'))
+
+    # Access header info
+    header = vcf_reader.metadata
+    sample_names = vcf_reader.samples
+
+    # Iterate over SNPs in VCF file
+    for record in vcf_reader:
+       for call in record.samples:
+          sample_name = call.sample
+          gt = call.gt_bases
