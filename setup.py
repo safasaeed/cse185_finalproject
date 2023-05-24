070d2190ce84f3c1c91eec0be8f7ccd23b6a6e0e
Honieh Hemati <133917865+honieh-hemati@users.noreply.github.com>
2023-05-24 14:37:57 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+import os
+from setuptools import setup, find_packages
+
+# version-keeping code based on pybedtools
+curdir = os.path.abspath(os.path.dirname(__file__))
+MAJ = 0
+MIN = 0
+REV = 0
+VERSION = '%d.%d.%d' % (MAJ, MIN, REV)
+with open(os.path.join(curdir, 'mygwas/version.py'), 'w') as fout:
+        fout.write(
+            "\n".join(["",
+                       "# THIS FILE IS GENERATED FROM SETUP.PY",
+                       "version = '{version}'",
+                       "__version__ = version"]).format(version=VERSION)
+        )
+
+
+setup(
+    name='mygwas',
+    version=VERSION,
+    description='CSE185 Demo Project',
+    author='Honieh Hemati, Saara Kriplani, Safa Saeed',
+    author_email='hhemati@ucsd.edu',
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "mygwas=mygwas.mygwas:main"
+        ],
+    },
+)
